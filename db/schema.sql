7704f1e2828f2a63089020b654e4f9858f93f051
aldoibuado <aldo.ibuado@gmail.com>
2022-12-07 12:38:03 -0800

    created table in regards to employees

--- db/schema.sql
+++ db/schema.sql
@@ -20,3 +20,16 @@ CREATE TABLE roles (
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE employees (
+    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id: INT NOT NULL,
+    manager_id: INT NULL,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE, 
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL ON UPDATE CASCASE
+);
