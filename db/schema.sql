e69f1dc111d45fc9afada3a859af75dc3a5e1642
aldoibuado <aldo.ibuado@gmail.com>
2022-12-07 12:08:59 -0800

    created the table regarding department roles

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,20 @@ DROP DATABASE IF EXISTS employees_db;
 -- will create employee database
 CREATE DATABASE employees_db;
 -- will use the employee database
-USE employee_db;
+USE employees_db;
 
 CREATE TABLE department (
     id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_name: VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title: VARCHAR(30) NOT NULL,
+    salary: DECIMAL(8,4) NULL,
+    department_id: INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
