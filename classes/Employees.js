da4162a395f593b45c7456ddc52aa4b5b1a1f7dd
aldoibuado <aldo.ibuado@gmail.com>
2022-12-08 09:47:32 -0800

    added Employees constructor

---
+++ classes/Employees.js
@@ -0,0 +1,11 @@
+// Employees constructor
+class Employees {
+    constructor(first_name, last_name, role_id, manager_id) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.role_id = role_id;
+        this.manager_id = manager_id;
+    }
+};
+
+module.exports = Employees;
