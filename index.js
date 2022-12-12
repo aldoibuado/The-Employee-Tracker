720dad522be9484356a92db02bf6a8ffc5db7b92
aldoibuado <aldo.ibuado@gmail.com>
2022-12-11 16:11:49 -0800

    added functions to help add a department and new role

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function chooseAction() {
         "Add a role",
         "Add an employee",
         "Update employee role",
+        "Delete a department",
         "Quit",
       ],
     })
@@ -66,6 +67,10 @@ function chooseAction() {
           updateRole();
           break;
 
+        case "Delete a department" :
+            deleteDepartment();
+            break;
+
         case "Quit":
           db.end();
           break;
@@ -74,7 +79,7 @@ function chooseAction() {
           console.log(choice);
       }
     });
-}
+};
 
 // Query to get information regarding the department from the employees database
 function viewDepartments() {
@@ -85,7 +90,7 @@ function viewDepartments() {
     console.table(res);
     chooseAction();
   });
-}
+};
 
 // Query to get information regarding roles from the employees database
 function viewRoles() {
@@ -96,20 +101,80 @@ function viewRoles() {
     console.table(res);
     chooseAction();
   });
-}
+};
 
 // Query to get information regarding all employees from the employees database
 function viewEmployees() {
-  db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title, department_name, roles.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager 
+  db.query(
+    `SELECT employees.id, employees.first_name, employees.last_name, roles.title, department_name, roles.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager 
   FROM employees 
   INNER JOIN roles on roles.id = employees.role_id 
   INNER JOIN department on department.id = roles.department_id 
-  LEFT JOIN employees e on employees.manager_id = e.id;`, (err, res) => {
-    if (err) {
-      throw err;
+  LEFT JOIN employees e on employees.manager_id = e.id;`,
+    (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      chooseAction();
     }
-    console.table(res);
-    chooseAction();
-  });
-}
+  );
+};
+
+// helps with giving the user the option to add a department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "What is the name of the department"
+        }
+    ])
+    .then(res => {
+        db.query(`INSERT INTO department (department_name) VALUES (?)`,
+        [res.department],
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+            console.log("Added department");
+            chooseAction();
+        }
+      );
+    });
+};
+
+// helps user with inputting the title name, salary and department of the new role
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of the role",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary of the role?",
+        },
+        {
+            type: "input",
+            name: "departmentID",
+            message: "What department does the role belong to?"
+        }
+    ])
+    .then(res => {
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`,
+        [res.rolestitle, res.salary, res.department_id],
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+            console.log("Added role");
+            chooseAction();
+        }
+      );
+    });
+};
+
 
